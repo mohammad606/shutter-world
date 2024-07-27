6beb7ff47ecc1859d2b6ef9929c746cb62eb1387
محمد halabi <mohammad123alhalabi123@gmail.com>
2024-07-27 14:03:31 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+    screens: {
+      'sm': {'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px'},
+
+    },
+  },
+  plugins: [],
+}
